2776c2eaddcc47ca3c5767da9788889784db48e1
Jason Noble <github+jasonn@jasonnoble.org>
2012-12-31 10:37:15 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentConsolePractice::Application.config.secret_token = '52621bbc961936a364a2fd25768111087c8187738f91c353c1cb887f98e5789537aaeaf85c5b9e02e83fbef7a23e2688e26b22936ff44539fb4952ebfe931f7f'
